e917693d083e32c544c8c5fb69533ec8ff86f565
xushier <d_songbo@126.com>
2022-08-04 15:43:59 +0800

    chg

--- README.md
+++ README.md
@@ -1,22 +1,25 @@
-# Unraid Nas 的一些容器安装模板
+# 自用 Unraid Nas 容器安装模板获取脚本
 
-- 模板内置另外一个项目的高清图标。
+- 模板内置另外一个项目的高清图标。图标项目地址：[HD-Icons](https://github.com/xushier/HD-Icons)  [HD-Icons](https://hub.fastgit.xyz/xushier/HD-Icons)
 - 脚本会将图标下载到`/boot/config/HD-Icons/`并将容器图标替换。
-- 图标项目地址：[HD-Icons](https://github.com/xushier/HD-Icons)
 
 # 使用方法
-在 unraid 命令行执行：
+在 unraid 命令行 Ctrl+Shift+V 粘贴以下内容：
 ```
 wget -qN https://raw.githubusercontent.com/xushier/Unraid-Docker-Templates/main/choices.sh && sh choices.sh
 ```
 
-或者没有梯子的话使用镜像地址：
+如果没有梯子的话使用加速地址：
+```
+wget -qN https://ghproxy.com/https://raw.githubusercontent.com/xushier/Unraid-Docker-Templates/main/choices.sh && sh choices.sh
+```
+或者镜像地址：
 ```
 wget -qN https://raw.fastgit.org/xushier/Unraid-Docker-Templates/main/choices.sh && sh choices.sh
 ```
-根据序号选择对应的模板，完成之后在 Unraid Docker 界面添加容器，找到并选择模板，根据实际需要修改部分参数即可。
+根据提示选择对应的模板，完成之后在 Unraid Docker 界面添加容器，找到并选择模板，根据实际需要修改部分参数即可。
 
-# 更新
+# 脚本更新
 - 传入参数 2 更新脚本：`sh choices.sh 2`
 - 传入参数 1 使用镜像地址：`sh choices.sh 1`
 
---
+++ choices_compose.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+declare -A container template icon
+xdtx_icon_dir="/boot/config/HD-Icons"
+xdtx_template_dir="/boot/config/plugins/dockerMan/templates-user"
+raw_domain="https://raw.githubusercontent.com"
+raw_mirror="https://raw.fastgit.org"
+
+if [[ $1 == 1 ]];then domain=$raw_mirror;echo -e "\n使用 Github 镜像加速地址\n";else domain=$raw_domain;fi
+if [[ $1 == 2 ]];then curl https://raw.githubusercontent.com/xushier/Unraid-Docker-Templates/main/choices.sh > choices.sh;fi
+
+container=(\
+[1]="【密码管理器】Vaultwarden_Compose 版" \
+)
+
+template=(\
+[1]="Vaultwarden_Compose" \
+)
+
+icon=(\
+[1]="Bitwarden" \
+)
+
+while :
+do
+	echo -e "\n传入参数 1，使用镜像地址。例：sh choices.sh 1\n传入参数 2，更新脚本。例：sh choices.sh 2\n"
+	for key in $(seq ${#container[@]})
+	do
+	    echo "$key : ${container[$key]}"
+	done
+	
+	str=$'\n'
+	hr="\n--------------------------------------------------------------------------------"
+    
+	read -p "$str请选择模板序号（1到${#container[@]}，回车直接退出）：" container_num
+	if [ $container_num == $str ];then
+		echo -e "\n手动退出......"
+		break
+	fi
+
+	read -p "$str你选择的是：${container[$container_num]}，确定吗?（是输入 y，否输入 n，回车直接退出）" conf
+	if [[ $conf == "y" ]] || [[ $conf == "Y" ]];then
+		mkdir -p $xdtx_icon_dir && cd $xdtx_icon_dir && echo -e "$hr\n开始下载图标文件\n路径 $xdtx_icon_dir/${icon[$container_num]}.png\n" && curl -O "$domain/xushier/HD-Icons/main/border-radius/${icon[$container_num]}.png" && echo -e "\n图标下载完毕。$hr"
+		cd $xdtx_template_dir && echo -e "$hr\n开始下载模板文件\n路径 $xdtx_template_dir/my-${template[$container_num]}.xml\n"&& curl -O "$domain/xushier/my-unraid-docker-templates/main/templates/my-${template[$container_num]}.xml" && sed -i "s/<Icon>.*<\/Icon>/<Icon>${xdtx_icon_dir//\//\\\/}\/${icon[$container_num]}.png<\/Icon>/g" "$xdtx_template_dir/my-${template[$container_num]}.xml" && echo -e "\n模板文件下载完毕。在容器界面点击添加容器，选择该模板即可。$hr"
+		echo -e "$hr\n图标库：https://github.com/xushier/HD-Icons\n公众号：小迪同学\n B 站：煦诗儿$hr\n"
+		break
+	fi
+	if [[ $conf == "n" ]] || [[ $conf == "N" ]];then
+		echo -e "\n )~!~( 这都能输错，小迪一脸嫌弃 )~!~(\n"
+		continue
+	fi
+	echo -e "\n手动退出......"
+	break
+done
